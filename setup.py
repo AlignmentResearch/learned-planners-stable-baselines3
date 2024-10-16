dcf7fa4d82958e52e2a64e7312eba1607860bd70
taufeeque9 <9taufeeque9@gmail.com>
2024-10-16 16:44:04 -0700

    update mamba_lens

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
         # Plotting learning curves
         "matplotlib",
         # For hooks on recurrent policy
-        "mamba_lens @ git+https://github.com/Phylliida/MambaLens.git",
+        "mamba_lens @ git+https://github.com/taufeeque9/MambaLens.git",
     ],
     extras_require={
         "tests": [
